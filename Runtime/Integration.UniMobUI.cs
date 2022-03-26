47f6493064848a540e18e53cca7ec51db721957d
VladV <vanifatov.vlad@yandex.ru>
2022-03-26 13:43:16 +0300

    Add UniMob.UI integration

---
+++ Runtime/Integration.UniMobUI.cs
@@ -0,0 +1,51 @@
+#if UNIMOB_UI
+
+using System;
+using JetBrains.Annotations;
+using UniMob.UI;
+
+namespace CodeWriter.ViewBinding
+{
+    public static class ViewVariableExtensions
+    {
+        [PublicAPI]
+        public static void Bind<T, TVariable, TState>(this ViewVariable<T, TVariable> variable,
+            ViewBindable<TState> it, Func<T> f)
+            where TState : class, IViewState
+            where TVariable : ViewVariable<T, TVariable>
+        {
+            var activation = new Action(() => variable.SetSource(f));
+            var deactivation = new Action(() => variable.SetSource(() => default));
+
+            it.AddActivationCallback(activation);
+            it.AddDeactivationCallback(deactivation);
+        }
+
+        [PublicAPI]
+        public static void Bind<T, TEvent, TState>(this ViewParametrizedEvent<T, TEvent> evt,
+            ViewBindable<TState> it, Action<T> f)
+            where TState : class, IViewState
+            where TEvent : ViewParametrizedEvent<T, TEvent>
+        {
+            var activation = new Action(() => evt.AddListener(f));
+            var deactivation = new Action(() => evt.RemoveListener(f));
+
+            it.AddActivationCallback(activation);
+            it.AddDeactivationCallback(deactivation);
+        }
+
+        [PublicAPI]
+        public static void Bind<TState>(this ViewEventVoid evt,
+            ViewBindable<TState> it, Action f)
+            where TState : class, IViewState
+        {
+            var activation = new Action(() => evt.AddListener(f));
+            var deactivation = new Action(() => evt.RemoveListener(f));
+
+            it.AddActivationCallback(activation);
+            it.AddDeactivationCallback(deactivation);
+        }
+    }
+}
+
+#endif
