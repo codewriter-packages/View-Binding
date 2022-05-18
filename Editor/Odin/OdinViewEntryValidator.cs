f47c0ca87f47ec4185d45d6f4c4f02a4c2b9ef07
VladV <vanifatov.vlad@yandex.ru>
2022-05-18 13:38:12 +0300

    Fix compilation error with Odin Inspector 3.1

--- Editor/Odin/OdinViewEntryValidator.cs
+++ Editor/Odin/OdinViewEntryValidator.cs
@@ -10,7 +10,19 @@ namespace CodeWriter.ViewBinding.Editor.Odin
     public class OdinViewEntryValidator<TViewEntry> : ValueValidator<TViewEntry>
         where TViewEntry : ViewEntry
     {
+#if ODIN_INSPECTOR_3_1
+        protected override void Validate(ValidationResult result)
+        {
+            ValidateInternal(ValueEntry.SmartValue, result);
+        }
+#else
         protected override void Validate(TViewEntry value, ValidationResult result)
+        {
+            ValudateInternal(value, result);
+        }
+#endif
+
+        private void ValidateInternal(TViewEntry value, ValidationResult result)
         {
             var message = value.GetErrorMessage();
             if (message == null)
