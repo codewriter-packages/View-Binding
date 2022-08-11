03adcd3c8358b3d46eae954eca3728e7e3265b0d
VladV <vanifatov.vlad@yandex.ru>
2022-08-11 11:58:04 +0300

    Add CanvasGroup applicators

---
+++ Runtime/Applicators/UI/CanvasGroupInteractableApplicator.cs
@@ -0,0 +1,18 @@
+﻿using CodeWriter.ViewBinding.Applicators;
+using UnityEngine;
+
+namespace CodeWriter.ViewBinding.Extras
+{
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(CanvasGroup))]
+    [AddComponentMenu("View Binding/UI/[Binding] Canvas Interactable Applicator")]
+    public class CanvasGroupInteractableApplicator : ComponentApplicatorBase<CanvasGroup, ViewVariableBool>
+    {
+        [SerializeField] private bool inverse = false;
+
+        protected override void Apply(CanvasGroup target, ViewVariableBool source)
+        {
+            target.interactable = source.Value != inverse;
+        }
+    }
+}
---
+++ Runtime/Applicators/UI/CanvasGroupRaycastTargetApplicator.cs
@@ -0,0 +1,18 @@
+﻿namespace CodeWriter.ViewBinding.Extras
+{
+    using Applicators;
+    using UnityEngine;
+
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(CanvasGroup))]
+    [AddComponentMenu("View Binding/UI/[Binding] Canvas RaycastTarget Applicator")]
+    public class CanvasGroupRaycastTargetApplicator : ComponentApplicatorBase<CanvasGroup, ViewVariableBool>
+    {
+        [SerializeField] private bool inverse = false;
+
+        protected override void Apply(CanvasGroup target, ViewVariableBool source)
+        {
+            target.blocksRaycasts = source.Value != inverse;
+        }
+    }
+}
