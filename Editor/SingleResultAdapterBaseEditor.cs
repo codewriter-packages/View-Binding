2ef47982d80d858e1ec6a6eb77fdd47f07bade2e
VladV <vanifatov.vlad@yandex.ru>
2023-03-21 18:44:38 +0400

    Add extraContexts drawer for Adapter

--- Editor/SingleResultAdapterBaseEditor.cs
+++ Editor/SingleResultAdapterBaseEditor.cs
@@ -7,27 +7,37 @@ namespace CodeWriter.ViewBinding.Editor
     public class SingleResultAdapterBaseEditor : UnityEditor.Editor
     {
         private const string ResultFieldName = "result";
+        private const string ExtraContextsFieldName = "extraContexts";
 
         private static readonly GUIContent AliasContent = new GUIContent("Alias");
 
         private static readonly string[] ExcludedProps =
         {
             "m_Script",
-            ResultFieldName
+            ResultFieldName,
+            ExtraContextsFieldName,
         };
 
         private SerializedProperty _resultProp;
         private SerializedProperty _resultNameProp;
+        private SerializedProperty _extraContextsProp;
 
         public virtual void OnEnable()
         {
             _resultProp = serializedObject.FindProperty(ResultFieldName);
             _resultNameProp = _resultProp.FindPropertyRelative("name");
+            _extraContextsProp = serializedObject.FindProperty(ExtraContextsFieldName);
         }
 
         public sealed override void OnInspectorGUI()
         {
             serializedObject.Update();
+
+            if (_extraContextsProp != null)
+            {
+                ViewContextGUI.DrawContextField(serializedObject, null, _extraContextsProp);
+                GUILayout.Space(10);
+            }
 
             DoGUI();
 
