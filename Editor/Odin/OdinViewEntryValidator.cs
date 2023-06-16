fb4c7a5d0ece6198ee55039e64e2cfb8fdf6b405
VladV <vanifatov.vlad@yandex.ru>
2023-06-16 20:57:24 +0400

    Odin validator

---
+++ Editor/Odin/OdinViewEntryValidator.cs
@@ -0,0 +1,39 @@
+﻿#if ODIN_INSPECTOR
+
+using CodeWriter.ViewBinding.Editor.Odin;
+using Sirenix.OdinInspector.Editor.Validation;
+
+[assembly: RegisterValidator(typeof(OdinViewEntryValidator<>))]
+
+namespace CodeWriter.ViewBinding.Editor.Odin
+{
+    public class OdinViewEntryValidator<TViewEntry> : ValueValidator<TViewEntry>
+        where TViewEntry : ViewEntry
+    {
+#if ODIN_INSPECTOR_3_1
+        protected override void Validate(ValidationResult result)
+        {
+            ValidateInternal(ValueEntry.SmartValue, result);
+        }
+#else
+        protected override void Validate(TViewEntry value, ValidationResult result)
+        {
+            ValidateInternal(value, result);
+        }
+#endif
+
+        private void ValidateInternal(TViewEntry value, ValidationResult result)
+        {
+            var message = value.GetErrorMessage();
+            if (message == null)
+            {
+                return;
+            }
+
+            result.Message = message;
+            result.ResultType = ValidationResultType.Error;
+        }
+    }
+}
+
+#endif
--- Runtime/Assembly.cs
+++ Runtime/Assembly.cs
@@ -2,4 +2,5 @@ using System.Runtime.CompilerServices;
 using TriInspector;
 
 [assembly: InternalsVisibleTo("CodeWriter.ViewBinding.Editor")]
+[assembly: InternalsVisibleTo("CodeWriter.ViewBinding.Editor.Odin")]
 [assembly: DrawWithTriInspector]
